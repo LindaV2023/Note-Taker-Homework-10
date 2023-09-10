e3cbbe829123daa0be7a6309ff121ee0dfed099c
Linda Vitrella <lvitrella@mail.com>
2023-09-10 19:44:08 -0400

    updated all files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,34 @@
+//const fs = required('fs');
+const path = required('path');
+
+app.get('/api/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+} );
+
+app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync;
+    res.json(db);
+
+    let userNote = {
+        title: req.body.title,
+        text: req.body.text,
+
+
+    };
+
+db.push(userNote);
+fs.writeFileSync('db/db.json', JSON.stringify(db))
+res.json(db);
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let db =JSON.parse(fs.readFileSync('db/db.json'))
+
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+
+    fs.writeFileSync('db/db.json'. JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+})
+
+module.exports = app;
